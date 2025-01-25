2b98f4d22fdc5186d7e7626a7c0165b67b9bebef
Muhindo Mubaraka <mubahood360@gmail.com>
2025-01-25 21:10:42 +0300

    Add routes for cache clearing and migration execution

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,24 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 
+Route::get('/clear', function () {
+
+    Artisan::call('cache:clear');
+    Artisan::call('config:clear');
+    Artisan::call('config:cache');
+    Artisan::call('view:clear');
+
+    return "Cleared!";
+});
+Route::get('migrate', function () {
+    //run laravel migrate command in code
+    $RESP = Artisan::call('migrate');
+    echo "<pre>";
+    print_r($RESP);
+    echo "</pre>";
+    die();
+});
+
 Route::get('mail-test', function () {
 
     /* 
@@ -47,9 +65,9 @@ Route::get('mail-test', function () {
     $order->service_amount =  '10';
     $order->weight =  '10';
 
-  
+
     $order->save();
-    die("done"); 
+    die("done");
 
     $url = url('mail-test');
     $from = env('APP_NAME') . " Team.";
